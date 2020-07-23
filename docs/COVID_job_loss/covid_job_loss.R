5343f5120cf3ea7b9cd1dfbf99e519b0a87f7635
Harrison Kirby <harrisonk.kirby@gmail.com>
2020-07-23 13:50:09 -0400

    Added internet maps

--- docs/COVID_job_loss/covid_job_loss.R
+++ docs/COVID_job_loss/covid_job_loss.R
@@ -7,22 +7,23 @@ library(glptools)
 
 library(leaflet)
 
-job_loss <- read_csv("COVID_job_loss/job_loss_by_tract.csv")
+job_loss_tract  <- read_csv("COVID_job_loss/job_loss_by_tract.csv")
+job_loss_county <- read_csv("COVID_job_loss/sum_job_loss_county.csv")
 
-job_loss %<>%
+job_loss_tract %<>%
   filter(county_fips == "21111") %>%
   transmute(
     tract = GEOID,
     loss_rate = li_worker_unemp_rate * 100)
 
-write_csv(job_loss, "output_data/job_loss_rate.csv")
+write_csv(job_loss_tract, "output_data/job_loss_rate.csv")
 
-make_map(job_loss_object, loss_rate)
-
-
-
-job_loss_object@data %<>% left_join(job_loss, by = "tract")
-
-save(job_loss_object, file = "output_data/job_loss_object.RData")
-
-
+job_loss_county %<>%
+  transmute(
+    FIPS = county_fips,
+    loss_rate = low_income_worker_job_loss_rate * 100,
+    total = X000) %>%
+  pull_peers(add_info = F) %>%
+  stl_merge(loss_rate, weight_var = "total")
+  
+write_csv(job_loss_county, "output_data/job_loss_county.csv")
